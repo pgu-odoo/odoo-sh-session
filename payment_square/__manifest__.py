ce293fe14d0e0f3e079240b57ba7f718fb7f2dde
Pratima Gupta <pgu@odoo.com>
2024-11-22 13:17:21 +0530

    [IMP] added new module

---
+++ payment_square/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Payment Provider: Square',
+    'category': 'Accounting/Payment Providers',
+    'sequence': 357,
+    'summary': 'Payment Provider: Square',
+    'version': '18.0.0.1.0',
+    'description': """Square Payment Systems""",
+    'depends': ['website_sale', 'payment'],
+    'data': [
+    ],
+    'assets': {
+    },
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
